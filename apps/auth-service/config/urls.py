e798c915d988c8068f2499fb0ebe52fa6888bf1c
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2022-05-07 14:05:00 +0300

    boilerplate code for auth service

---
+++ apps/auth-service/config/urls.py
@@ -0,0 +1,27 @@
+import debug_toolbar
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from config.swagger import urlpatterns as swagger_urlpattern
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
+
+if settings.DEBUG:
+    urlpatterns += [
+        *swagger_urlpattern,
+        *static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
+        *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
+    ]
+
+if settings.TOOLBAR:
+    urlpatterns += [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ]
