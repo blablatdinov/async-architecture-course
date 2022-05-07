b438a533771aa5e20e75b2c69942a391203466a5
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2022-05-07 14:53:28 +0300

    share account type in token

--- apps/auth-service/tests/test_auth_jwt.py
+++ apps/auth-service/tests/test_auth_jwt.py
@@ -1,5 +1,8 @@
+import jwt
 import pytest
+from django.conf import settings
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
 
 pytestmark = [pytest.mark.django_db]
 
@@ -7,17 +10,21 @@ User = get_user_model()
 
 
 @pytest.fixture(autouse=True)
-def user(mixer):
-    user = mixer.blend(User, username='hp')
+def popug_user(mixer):
+    user = mixer.blend(User, username='popug1')
     user.set_password('1')
     user.save()
+    user.groups.add(Group.objects.get(name='popug'))
     return user
 
 
 def test(client):
     got = client.post('/api/token/', data={
-        'username': 'hp',
+        'username': 'popug1',
         'password': '1',
     })
+    decoded_token = jwt.decode(got.json()['access'], settings.SECRET_KEY, algorithms=['HS256'])
 
     assert got.status_code == 200
+    assert list(decoded_token.keys()) == ['token_type', 'exp', 'iat', 'jti', 'user_id', 'account_type']
+    assert decoded_token['account_type'] == 'popug'
