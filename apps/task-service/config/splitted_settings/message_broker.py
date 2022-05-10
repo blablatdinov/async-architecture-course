8b5b3d648d8b0af3605ef7236f910beb8f016af6
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2022-05-10 14:14:07 +0300

    first version Task.Added event

--- apps/task-service/config/splitted_settings/message_broker.py
+++ apps/task-service/config/splitted_settings/message_broker.py
@@ -1,6 +1,25 @@
+import json
+
 import pika
+from event_schema_registry import validate_schema
 
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 RABBITMQ_CHANNEL = connection.channel()
 
 RABBITMQ_CHANNEL.queue_declare(queue='popug')
+
+
+def publish_event(body):
+    try:
+        validate_schema(body, body['event_name'], body['event_version'])
+    except TypeError as e:
+        # some notification
+        raise e
+    RABBITMQ_CHANNEL.basic_publish(
+        exchange='',
+        routing_key='popug',
+        body=json.dumps(body),
+    )
+
+
+RABBITMQ_CHANNEL.publish_event = publish_event
--- apps/task-service/tasks/views.py
+++ apps/task-service/tasks/views.py
@@ -1,3 +1,7 @@
+import datetime
+import uuid
+
+from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.urls import path
 from rest_framework import serializers
@@ -29,13 +33,31 @@ class TasksView(APIView):
             TaskSerializer(Task.objects.filter(executor=request.user), many=True).data,
         )
 
+    def _publish_event(self, task: Task):
+        settings.RABBITMQ_CHANNEL.publish_event(
+            {
+                "event_id": str(uuid.uuid4()),
+                "event_version": 1,
+                "event_name": "Task.Added",
+                "event_time": str(datetime.datetime.now().timestamp()),
+                "producer": "task service",
+                "data": {
+                    "public_id": str(task.pk),
+                    "title": task.title,
+                    "description": task.description,
+                    "executor_id": str(task.executor_id),
+                },
+            },
+        )
+
     def post(self, request):
         executor = User.objects.order_by('?').first()
-        Task.objects.create(
+        task = Task.objects.create(
             executor=executor,
             title=request.data['title'],
             description=request.data['description'],
         )
+        self._publish_event(task)
         return Response(
             {'executor': executor.username, 'title': request.data['title'], 'description': request.data['description']},
             status=201,
