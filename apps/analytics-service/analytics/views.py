64a35c73e2dcf8bb8d29db0add4645a8fe67bb71
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2022-05-15 11:06:41 +0300

    Analytics: endpoint for getting count of popug with negative balance

--- apps/analytics-service/analytics/views.py
+++ apps/analytics-service/analytics/views.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from analytics.models import ManagementAward
+from analytics.models import ManagementAward, CountPopugWithNegativeAward
 
 
 class ManagementAwardView(APIView):
@@ -23,6 +23,21 @@ class ManagementAwardView(APIView):
         return Response(management_award, status=200)
 
 
+class PopugWithNegativeAwardCount(APIView):
+
+    def get(self, request):
+        now_date = timezone.now()
+        date = datetime.date(now_date.year, now_date.month, now_date.day)
+        count_popug_with_negative_award = (
+            CountPopugWithNegativeAward.objects
+            .filter(date=date)
+            .first()
+            .award
+        )
+        return Response(count_popug_with_negative_award, status=200)
+
+
 urlpatterns = [
-    path('api/v1/analytics/management-award/', ManagementAwardView.as_view())
+    path('api/v1/analytics/management-award/', ManagementAwardView.as_view()),
+    path('api/v1/analytics/popug-with-negative-award-count/', PopugWithNegativeAwardCount.as_view()),
 ]
